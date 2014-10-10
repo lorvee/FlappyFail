d52d308b1d0792620dff5b210cf8ee685975af36
lorvee <lor.vee@gmail.com>
2014-10-10 14:00:01 -0500

    Lo intentamos.
    Pero no pudo funcionar.

---
+++ Assets/Scripts/Flappy.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Flappy : MonoBehaviour {
+
+	public float YSpeed = 5;
+	public float XSpeed = 5;
+	public float FlyingPower = 20;
+	public Main MainGame;
+	// Use this for initialization
+	void Start () {
+		//Debug.Log("Trigger: " + collider.isTrigger);
+		MainGame.FlappyStartPosition = gameObject.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float translateX = XSpeed * Time.deltaTime;
+		float translateY = 0;
+		if(Input.GetKey(KeyCode.Space)) {
+			translateY = FlyingPower * Time.deltaTime;
+		}
+		translateY -= YSpeed * Time.deltaTime;
+		gameObject.transform.Translate( new Vector3(translateX, translateY, 0));
+	}
+	//function OnCollisionEnter2D(Collision2D collision)
+	void OnCollisionEnter2D(Collision2D collision)
+	{
+		//if(collision.gameObject.tag == "Tubo")
+				MainGame.SetGameOver();
+	}
+}
---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	private Vector3 _flappyStartPosition;
+	public Vector3 FlappyStartPosition{ set{ _flappyStartPosition = value; } }
+	
+	public Flappy FlappyCharacter;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	public void SetGameOver()
+	{
+		FlappyCharacter.gameObject.transform.position = _flappyStartPosition;
+		//FlappyCharacter.gameObject.rigidbody2D.velocity = new Vector2(0, 0);
+	}
+}
